8e3cf16c3ea6cdbceca00671d36a95d26ca0ada4
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2020-11-18 10:17:15 +0700

    Create MateriKe2.java

---
+++ materi ke 2/src/materi/ke/pkg2/MateriKe2.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package materi.ke.pkg2;
+
+/**
+ *
+ * @author AXZ
+ */
+public class MateriKe2 {
+
+    public static void main(String[] args) {
+      
+        int a = 80;
+        int b = 25;
+        
+        int jumlah = a+b;
+        int kurang = a-b;
+        int bagi = a/b;
+        int kali = a*b;
+        int mod = a%b;
+        
+        System.out.println("Hasil Penjumlahan = "+jumlah);
+        System.out.println("Hasil Pengurangan = "+kurang);
+        System.out.println("Hasil Pembagian = "+bagi);
+        System.out.println("Hasil Perkalian = "+kali);
+        System.out.println("Hasil Modulo = "+mod);
+    }
+    
+}
