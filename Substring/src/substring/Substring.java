fe727420644ef8c35dc577b2ad72b2ac389df160
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2020-12-09 07:43:11 +0700

    Create Substring.java

---
+++ Substring/src/substring/Substring.java
@@ -0,0 +1,20 @@
+package substring;
+
+/**
+ *
+ * @author AXZ
+ */
+public class Substring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        String var_a = "hallo world";
+        String var_b = var_a.substring(1,4);
+        System.out.println(var_a);
+        System.out.println(var_b);
+    }
+    
+}
