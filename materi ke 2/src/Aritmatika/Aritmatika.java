5083a2de7a561cb9a041f7b92f141fb489ac836f
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2020-12-09 07:43:36 +0700

    Create Aritmatika.java

---
+++ materi ke 2/src/Aritmatika/Aritmatika.java
@@ -0,0 +1,39 @@
+package Aritmatika;
+
+public class Aritmatika {
+
+    public static void main(String[] args) {
+      
+        double a = 100;
+        double b = 23;
+        
+        double tambah = a/b;
+        
+        float a1 = 100;
+        float b1 = 23;
+        
+        float bagi = a1/b1;
+        
+        String kata = "ini kata 901!!@#23";
+        char karakter = '8';
+        
+        int x = 100;
+        int y = 23; 
+        
+        int interger = x/y;
+        
+        int p = 100;
+        int q = 20;
+        int modulusPQ = p%q;
+        int modulusXY = x%y;
+        
+        System.out.println("Hasil Bagi int = "+interger);
+        System.out.println("Hasil Bagi double = "+tambah);
+        System.out.println("Hasil Bagi mpodulus p%q = "+modulusPQ);
+        System.out.println("Hasil Bagi modulus x%y = "+modulusXY);
+        System.out.println("Hasil Bagi float = "+bagi);
+        System.out.println("Output String = "+kata);
+        System.out.println("Output char = "+karakter);
+    }
+    
+}
