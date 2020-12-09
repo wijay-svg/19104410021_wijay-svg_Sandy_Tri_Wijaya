e5c7968272ab65c337e392b4062a3c2bde75f59a
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2020-12-09 07:43:22 +0700

    Create T_5.java

---
+++ String/src/String_Case/T_5.java
@@ -0,0 +1,17 @@
+package String_Case;
+
+
+public class T_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        String var_a = "Hari ini saya makan Soto Ayam ";
+        String var_b = "minumnya Cola ";
+        System.out.println(var_a.toUpperCase());
+        System.out.println(var_a.toLowerCase());
+        System.out.println(var_a+var_b);
+    }
+}
