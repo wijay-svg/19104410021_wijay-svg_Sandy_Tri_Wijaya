2e4311ddfdbb06dc79bde098002195bed6ce8667
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2021-01-15 09:43:07 +0700

    Update tance.java

--- Inherin/src/inherin/tance.java
+++ Inherin/src/inherin/tance.java
@@ -20,8 +20,27 @@ public class tance extends Inherin
         String input2 = penerbit;
         String tahun = JOptionPane.showInputDialog("Masukan Tahun Terbit");
         String input3 = tahun;
+        String kate = JOptionPane.showInputDialog("Masukan Kategori");
+        String input4 = String.valueOf(kate);
+        String kategori = null;
+        if (input4.equalsIgnoreCase("SU"))
+        {
+            kategori = "Semua Umur";
+        }
+        else if(input4.equalsIgnoreCase("R"))
+        {
+            kategori = "Remaja"; 
+        }
+        else if(input4.equalsIgnoreCase("D"))
+        {
+            kategori = "Dewasa"; 
+        }
+        else if(input4.equalsIgnoreCase("A"))
+        {
+            kategori = "Anak";
+        }
         tance turunan = new tance();
-        turunan.setBuku_sejarah(input,input1,input2,input3);
+        turunan.setBuku_sejarah(input,input1,input2,input3,kategori);
         Daftar_buku.add(turunan.getBuku_sejarah());
         
         String judul1 = JOptionPane.showInputDialog("Inputkan Judul Buku");
@@ -32,8 +51,27 @@ public class tance extends Inherin
         String inp2 = penerbit1;
         String tahun1 = JOptionPane.showInputDialog("Masukan Tahun Terbit");
         String inp3 = tahun1;
+        String kat = JOptionPane.showInputDialog("Masukan Kategori");
+        String inp4 = String.valueOf(kat);
+        String kategori1 = null;
+        if (inp4.equalsIgnoreCase("SU"))
+        {
+            kategori1 = "Semua Umur";
+        }
+        else if(inp4.equalsIgnoreCase("R"))
+        {
+            kategori1 = "Remaja"; 
+        }
+        else if(inp4.equalsIgnoreCase("D"))
+        {
+            kategori1 = "Dewasa"; 
+        }
+        else if(inp4.equalsIgnoreCase("A"))
+        {
+            kategori1 = "Anak";
+        }
         tance turunan1 = new tance();
-        turunan1.setBuku_teknologi(inp,inp1,inp2,inp3);
+        turunan1.setBuku_teknologi(inp,inp1,inp2,inp3,kategori1);
         Daftar_buku.add(turunan1.getBuku_teknologi());
         
         String judul2 = JOptionPane.showInputDialog("Inputkan Judul Buku");
@@ -44,8 +82,27 @@ public class tance extends Inherin
         String in2 = penerbit2;
         String tahun2 = JOptionPane.showInputDialog("Masukan Tahun Terbit");
         String in3 = tahun2;
+        String katego = JOptionPane.showInputDialog("Masukan Kategori");
+        String in4 = String.valueOf(katego);
+        String kategori2 = null;
+        if (in4.equalsIgnoreCase("SU"))
+        {
+            kategori2 = "Semua Umur";
+        }
+        else if(in4.equalsIgnoreCase("R"))
+        {
+            kategori2 = "Remaja"; 
+        }
+        else if(in4.equalsIgnoreCase("D"))
+        {
+            kategori2 = "Dewasa"; 
+        }
+        else if(in4.equalsIgnoreCase("A"))
+        {
+            kategori2 = "Anak";
+        }
         tance turunan2 = new tance();
-        turunan2.setBuku_bisnis(in,in1,in2,in3);
+        turunan2.setBuku_bisnis(in,in1,in2,in3,kategori2);
         Daftar_buku.add(turunan2.getBuku_bisnis());
         System.out.println(Daftar_buku);
     }
