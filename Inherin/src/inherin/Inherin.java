0ef8606e09f94f8a53e0483d00621d5431bd7ca5
wijay-svg <71633317+wijay-svg@users.noreply.github.com>
2021-01-15 09:57:53 +0700

    Update Inherin.java

--- Inherin/src/inherin/Inherin.java
+++ Inherin/src/inherin/Inherin.java
@@ -21,11 +21,12 @@ public class Inherin {
 
     public void setBuku_sejarah(String judul,String pengarang,String penerbit,String tahun,String kategori) {
         this.buku_sejarah = ("Buku Sejarah \n\n" +
-                "Judul : " + judul+
-                "\nNama Pengarang : " + pengarang +
-                "\nPenerbit : " + penerbit +
-                "\nTahun Cetak : " + tahun + 
-                "\nKategori : " + kategori); 
+                "Judul \t\t\t: " + judul+
+                "\nNama Pengarang \t\t: " + pengarang +
+                "\nPenerbit   \t\t: " + penerbit +
+                "\nTahun Cetak\t\t: " + tahun + 
+                "\nKategori   \t\t: " + kategori +
+                "\n______________________________________"); 
                 
     }
 
@@ -35,11 +36,12 @@ public class Inherin {
 
     public void setBuku_teknologi(String judul1,String pengarang1,String penerbit1,String tahun1,String kategori1) {
         this.buku_teknologi = ( "\n\nBuku Teknologi \n\n" +
-                "Judul : " + judul1+
-                "\nNama Pengarang : " + pengarang1 +
-                "\nPenerbit : " + penerbit1 +
-                "\nTahun Cetak : " + tahun1 +
-                "\nKategori : " + kategori1);
+                "Judul \t\t\t: " + judul1+
+                "\nNama Pengarang \t\t: " + pengarang1 +
+                "\nPenerbit   \t\t: " + penerbit1 +
+                "\nTahun Cetak\t\t: " + tahun1 +
+                "\nKategori   \t\t: " + kategori1 + 
+                "\n______________________________________");
     }
 
     public String getBuku_bisnis() {
@@ -48,10 +50,11 @@ public class Inherin {
 
     public void setBuku_bisnis(String judul2,String pengarang2,String penerbit2,String tahun2,String kategori2) {
         this.buku_bisnis = ( "\n\nBuku Bisnis \n\n" +
-                "Judul : " + judul2+
-                "\nNama Pengarang : " + pengarang2 +
-                "\nPenerbit : " + penerbit2 +
-                "\nTahun Cetak : " + tahun2 +
-                "\nKategori : " + kategori2);
+                "Judul \t\t\t: " + judul2+
+                "\nNama Pengarang \t\t: " + pengarang2 +
+                "\nPenerbit   \t\t: " + penerbit2 +
+                "\nTahun Cetak\t\t: " + tahun2 +
+                "\nKategori   \t\t: " + kategori2 +
+                "\n______________________________________");
     }
 }
